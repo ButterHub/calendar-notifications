5d42997dbc2a3c94832f5968222457feeb220252
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2019-11-30 15:15:31 +0000

    No ford file

--- index.js
+++ index.js
@@ -26,10 +26,24 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 // time.
 const TOKEN_PATH = 'token.json';
 
+const listEvents = async () => {
+  await authorize(JSON.parse(credentials))
+  try {
+    const events = await getEvents();
+    console.log(events)
+  } catch (err) {
+    return console.log('The API returned an error: ' + err);
+  }
+}
+
 // Get list of all events with "CO" in the name. (Imperial computing events start with this). Save to file
-const credentials = fs.readFileSync('credentials.json')
-authorize(JSON.parse(credentials))
-listEvents()
+const filename = "credentials"
+const credentials = fs.readFileSync(filename + '.json')
+listEvents();
+
+// use gmaps api to get coordinates
+
+
 
 // Load file, and delete events with corresponding UIDS
 // const filesmap = JSON.parse(fs.readFileSync('uidsCorrect.json'))
@@ -51,7 +65,7 @@ listEvents()
  * @param {function} callback The callback to call with the authorized client.
  */
 async function authorize(credentials) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const {client_secret, client_id, redirect_uris} = credentials.installed// credentials.web;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
@@ -60,9 +74,9 @@ async function authorize(credentials) {
     token = await fs.readFileSync(TOKEN_PATH);
   } catch (e) {
     console.log("Token did not exist, get one from Google.")
-    return getAccessToken(oAuth2Client, callback);
+    return await getAccessToken(oAuth2Client); // returns nothing
   }
-  oAuth2Client.setCredentials(JSON.parse(token));;
+  return oAuth2Client.setCredentials(JSON.parse(token));; // returns nothing
   }
 
 /**
@@ -71,7 +85,7 @@ async function authorize(credentials) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
+async function getAccessToken(oAuth2Client) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -81,62 +95,42 @@ function getAccessToken(oAuth2Client, callback) {
     input: process.stdin,
     output: process.stdout,
   });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
+  return new Promise((res, rej) => {
+    rl.question('Enter the code from that page here: ', (code) => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log('Token stored to', TOKEN_PATH);
+          res();
+        });
       });
-      callback(oAuth2Client);
-    });
+  });
   });
 }
 
 /**
- * Lists the next 10 events on the user's primary calendar.
+ * Lists the next events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  const size = 1;
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: size,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming ' + size + ' events:');
-      events.map((event, i) => {
-        console.log(event)
-        // const start = event.start.dateTime || event.start.date;
-        // console.log(`${start} - ${event.summary}`);
+async function getEvents(auth, size) {
+    const calendar = google.calendar({version: 'v3', auth});
+    return new Promise((resolve, reject) => {
+      calendar.events.list({
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: size,
+        singleEvents: true,
+        orderBy: 'startTime',
+      }, (err, res) => {
+          if (err) return reject(err)
+          resolve(res.data.items);
       });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
+    })
 }
-
-const deleteEvent = UID => (auth) => {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.delete({
-    calendarId: 'primary',
-    eventId: UID
-  }, (err, res) => {
-    if (err) return console.log('The event delete API returned an error: ' + err);
-    console.log("Finished deleting 1 event")
-  });
-}
-// [END calendar_quickstart]
-
 
 module.exports = {
   SCOPES
